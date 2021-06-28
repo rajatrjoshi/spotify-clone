dee6cc00e1bc66767aa4f0584cd7c281cdd0c5fe
rajatrjoshi <rajatj969@gmail.com>
2021-06-28 20:35:07 +0530

    made login page

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+
+  const[token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token); 
+
+      spotify.getMe().then((user) => {
+        console.log('👨' , user);
+      })
+    }
+
+    
+
+  }, []);
+
+  return (
+    <div className="app">
+
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Player.css";
+
+function Player() {
+    return (
+        <div className="player">
+            <h1>Welcome To Spotify</h1>
+        </div>
+    )
+}
+
+export default Player;
