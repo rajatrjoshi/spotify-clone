685b7ed7d57a6f66d814fd9556d4ab3c215a1e1b
rajatrjoshi <rajatj969@gmail.com>
2021-07-01 11:18:57 +0530

    footer completed

--- src/Footer.js
+++ src/Footer.js
@@ -5,27 +5,43 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
-import { Shuffle } from '@material-ui/icons';
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
+
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <h1>Album and song details</h1>
+                <img className="footer__albumLogo" src="https://upload.wikimedia.org/wikipedia/en/1/12/Anne-Marie_and_Niall_Horan_-_Our_Song.png" alt=""/>
+                <div className="footer__songInfo">
+                    <h4>Yehhh!!</h4>
+                    <p>Usher</p>
+                </div>
             </div>
 
             <div className="footer__center">
                 <ShuffleIcon className="footer__green" />
                 <SkipPreviousIcon className="footer__icon" />
                 <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
-                <SkipNextIcon className="footer__green"/>
-                <RepeatIcon className="footer__icon"/>
+                <SkipNextIcon className="footer__icon"/>
+                <RepeatIcon className="footer__green"/>
             </div>
 
             <div className="footer__right">
-            
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
-
         </div>
     )
 }
