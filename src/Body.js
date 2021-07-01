17c5e445262dbaad69e68bbac1e946a69b79c588
rajatrjoshi <rajatj969@gmail.com>
2021-07-01 12:11:18 +0530

    banner added

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,14 @@ function Body({spotify}) {
     return (
         <div className="body">
             <Header spotify={spotify} />
+            <div className="body__info">
+                <img src="https://i.scdn.co/image/ab67706f000000039cc4931bc9b9e35128728127" alt="" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>This is Anne Marie</h2>
+                    <p>all songs of Anne Marie at a single place.... dont stop listening, dont stop simping</p>
+                </div>
+            </div>
         </div>
     )
 }
